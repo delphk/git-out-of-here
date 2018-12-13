a5a63c3c6774b995c5c5512078712825e0859938
delphinekoh <delphinekoh@hotmail.com>
2018-12-13 11:54:31 +0800

    change background color based on game status

--- src/Game.js
+++ src/Game.js
@@ -10,6 +10,12 @@ class Game extends Component {
     selectedIds: []
   };
 
+  static bgColors = {
+    playing: "#ccc",
+    won: "green",
+    lost: "red"
+  };
+
   challengeNumbers = Array.from({ length: this.props.challengeSize }).map(() =>
     randomNumberBetween(...this.props.challengeRange)
   );
@@ -20,19 +26,26 @@ class Game extends Component {
   ).reduce((acc, curr) => acc + curr);
 
   render() {
+    let { gameStatus, remainingSeconds } = this.state;
     return (
       <div className="game">
         <div className="help">
-          Pick 4 numbers that sum to the target in 15 seconds
+          Pick 4 numbers that sum to the target in {this.props.initialSeconds}{" "}
+          seconds
         </div>
-        <div className="target">{this.target}</div>
+        <div
+          className="target"
+          style={{ backgroundColor: Game.bgColors[gameStatus] }}
+        >
+          {this.target}
+        </div>
         <div className="challenge-numbers">
           {this.challengeNumbers.map((num, index) => (
-            <Number key={index} value={num} />
+            <Number key={index} id={index} value={num} />
           ))}
         </div>
         <div className="footer">
-          <div className="timer-value">{this.props.initialSeconds}</div>
+          <div className="timer-value">{remainingSeconds}</div>
           <button>Start</button>
         </div>
       </div>
